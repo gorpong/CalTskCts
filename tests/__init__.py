b994e3acb655cafc9dc3742cd8e45f898c6f309e
Gordon Galligher <gorpong@gmail.com>
2025-05-08 14:47:25 -0500

    Turn off logging when testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,9 @@
 import sys
 import os
+import logging
 
+# Set the path to find the classes I'm testing against
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+# Turn off logging as this is just testing
+logging.disable(logging.CRITICAL)
